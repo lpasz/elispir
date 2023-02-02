e7ca3f70685b8b65823ad4206d08952a54f665ca
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2023-02-02 14:37:53 -0300

    Some more useful commands

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,15 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-const createTerm
+let terminal: vscode.Terminal | null = null;
+
+const createTerminal = async () => {
+	if (!terminal) {
+		terminal = vscode.window.createTerminal("Integrated Terminal")
+		terminal.show(true)
+		terminal.sendText("iex")
+	}
+}
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -12,17 +20,58 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "elispir" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('elispir.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from elispir!');
+	let reload = vscode.commands.registerCommand('elispir.reload', async () => {
+		await createTerminal();
+		const editor = vscode.window.activeTextEditor;
+		if (editor) terminal?.sendText("reload", true)
+
 	});
 
-	context.subscriptions.push(disposable);
+	let sendText = vscode.commands.registerCommand('elispir.sendText', async () => {
+		await createTerminal();
+		const editor = vscode.window.activeTextEditor;
+		if (editor) {
+			var selection = editor.selection
+			var selectionText = editor.document.getText(selection)
+			if (selectionText) {
+				if (selectionText) terminal?.sendText(selectionText, true)
+			} else {
+				const { text } = editor?.document.lineAt(editor.selection.active.line);
+				if (text) terminal?.sendText(text, true);
+			}
+		}
+	});
+
+	let sendFile = vscode.commands.registerCommand('elispir.sendFile', async () => {
+		await createTerminal();
+		const editor = vscode.window.activeTextEditor;
+		if (editor) {
+			var fileText = editor.document.getText()
+			if (fileText) terminal?.sendText(fileText, true)
+		}
+	});
+
+	let sendAllLinesAbove = vscode.commands.registerCommand('elispir.sendAllLinesAbove', async () => {
+		await createTerminal();
+		const editor = vscode.window.activeTextEditor;
+		if (editor) {
+			const currentLine = editor.selection.active.line;
+			let allLinesAboveText = "";
+			for (let index = 0; index < currentLine; index++) {
+				const { text } = editor?.document.lineAt(index)
+				allLinesAboveText = allLinesAboveText + text + "\n";
+			}
+			terminal?.sendText(allLinesAboveText, true)
+			console.log({terminal})
+		}
+	});
+
+
+	context.subscriptions.push(sendText);
+	context.subscriptions.push(sendFile);
+	context.subscriptions.push(sendAllLinesAbove);
+	context.subscriptions.push(reload);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
