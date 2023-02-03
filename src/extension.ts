0f7e3ea533d96d477493b26deaa25e03d5c65558
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2023-02-03 12:26:04 -0300

    Show the results

--- src/extension.ts
+++ src/extension.ts
@@ -1,55 +1,55 @@
 import * as vscode from 'vscode';
+import { Range } from 'vscode';
 
 let terminal: vscode.Terminal | null = null;
+let decorators: vscode.DecorationOptions[] = []
 const config = vscode.workspace.getConfiguration('elispir');
-
-/// EXPERIMENTAL, STILL BROKEN.
-const getTerminalText = async () => {
-	let previousCopyPaste = await vscode.env.clipboard.readText()
-
-	await vscode.commands.executeCommand('workbench.action.terminal.selectToPreviousLine')
-	await vscode.commands.executeCommand('workbench.action.terminal.copySelection')
-	await vscode.commands.executeCommand('workbench.action.terminal.clearSelection')
-	await vscode.commands.executeCommand('workbench.action.terminal.')
-
-	let lastTerminalCommand = await vscode.env.clipboard.readText()
-
-	await vscode.env.clipboard.writeText(previousCopyPaste);
-
-	console.log({ lastTerminalCommand })
-}
-
-const createTerminal = async () => {
-	if (!terminal || terminal?.exitStatus) {
-		terminal = vscode.window.createTerminal("Integrated Terminal")
-		terminal.show(true)
-		const startCmd = config.get('iexCommand') as string
-		terminal.sendText(startCmd)
-	}
-}
-
-const getSelectedText = (editor: vscode.TextEditor): string => {
-	var selection = editor.selection
-	return editor.document.getText(selection)
-}
-
-const getCurrentLineText = (editor: vscode.TextEditor): string => {
-	const { text } = editor?.document.lineAt(editor.selection.active.line);
-	return text;
-}
-
-const getAllLinesAbove = (editor: vscode.TextEditor, currentLine: number): string => {
-	let allLinesAboveText = "";
-	for (let index = 0; index < currentLine; index++) {
-		const { text } = editor?.document.lineAt(index)
-		allLinesAboveText = allLinesAboveText + text + "\n";
-	}
-	return allLinesAboveText;
-}
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	let timeout: NodeJS.Timer | undefined = undefined;
+
+	// create a decorator type that we use to decorate small numbers
+	const smallNumberDecorationType = vscode.window.createTextEditorDecorationType({
+		borderWidth: '1px',
+		borderStyle: 'solid',
+		overviewRulerColor: 'blue',
+		overviewRulerLane: vscode.OverviewRulerLane.Right,
+		light: {
+			// this color will be used in light color themes
+			borderColor: 'darkblue'
+		},
+		dark: {
+			// this color will be used in dark color themes
+			borderColor: 'lightblue'
+		}
+	});
+
+	let activeEditor = vscode.window.activeTextEditor;
+
+	function updateDecorations() {
+		if (!activeEditor) {
+			return;
+		}
+		activeEditor.setDecorations(smallNumberDecorationType, decorators);
+		setTimeout(() => vscode.commands.executeCommand('editor.action.showHover'), 100);
+	}
+
+	function triggerUpdateDecorations(throttle = false) {
+		if (timeout) {
+			clearTimeout(timeout);
+			timeout = undefined;
+		}
+		if (throttle) {
+			timeout = setTimeout(updateDecorations, 500);
+		} else {
+			updateDecorations();
+		}
+	}
+
+	setInterval(triggerUpdateDecorations, 200)
+
 	let reload = vscode.commands.registerCommand('elispir.reload', async () => {
 		await createTerminal();
 		const editor = vscode.window.activeTextEditor;
@@ -67,9 +67,9 @@ export function activate(context: vscode.ExtensionContext) {
 			} else {
 				const currentLineText = getCurrentLineText(editor)
 				terminal?.sendText(currentLineText, true)
+				setTimeout(() => getTerminalText(editor), 100);
 			}
 
-			getTerminalText()
 		}
 	});
 
@@ -99,6 +99,51 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(sendFile);
 	context.subscriptions.push(sendAllLinesAbove);
 	context.subscriptions.push(reload);
+
+	const getTerminalText = async (editor: vscode.TextEditor) => {
+		let previousCopyPaste = await vscode.env.clipboard.readText()
+
+		await vscode.commands.executeCommand('workbench.action.terminal.focus')
+		await vscode.commands.executeCommand('workbench.action.terminal.selectToPreviousLine')
+		await vscode.commands.executeCommand('workbench.action.terminal.copySelection')
+		await vscode.commands.executeCommand('workbench.action.terminal.clearSelection')
+		await vscode.commands.executeCommand('workbench.action.terminal.scrollToBottom')
+		await vscode.commands.executeCommand('workbench.action.focusActiveEditorGroup')
+
+		const lastTerminalCommand = await (await vscode.env.clipboard.readText()).split("\n").at(-1)
+		console.log({ lastTerminalCommand })
+		await vscode.env.clipboard.writeText(previousCopyPaste)
+		const line = editor?.document.lineAt(editor.selection.active.line)
+		decorators = [{ range: line.range, hoverMessage: lastTerminalCommand }];
+	}
+
+	const createTerminal = async () => {
+		if (!terminal || terminal?.exitStatus) {
+			terminal = vscode.window.createTerminal("Integrated Terminal")
+			terminal.show(true)
+			const startCmd = config.get('iexCommand') as string
+			terminal.sendText(startCmd)
+		}
+	}
+
+	const getSelectedText = (editor: vscode.TextEditor): string => {
+		var selection = editor.selection
+		return editor.document.getText(selection)
+	}
+
+	const getCurrentLineText = (editor: vscode.TextEditor): string => {
+		const { text } = editor?.document.lineAt(editor.selection.active.line);
+		return text;
+	}
+
+	const getAllLinesAbove = (editor: vscode.TextEditor, currentLine: number): string => {
+		let allLinesAboveText = "";
+		for (let index = 0; index < currentLine; index++) {
+			const { text } = editor?.document.lineAt(index)
+			allLinesAboveText = allLinesAboveText + text + "\n";
+		}
+		return allLinesAboveText;
+	}
 }
 
 export function deactivate() { }
